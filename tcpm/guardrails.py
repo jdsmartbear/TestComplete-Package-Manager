24f672c417a1377142b826be7817c6de7202343c
Justin Dicroce <justin.dicroce@smartbear.com>
2026-01-06 20:12:13 -0500

    final fix to xml indentation

--- tcpm/guardrails.py
+++ tcpm/guardrails.py
@@ -10,7 +10,7 @@ from tcpm.download import downloadFile
 # ============================================================
 
 STORES_CHILD_LINE = (
-    '\t\t<child name="Stores" '
+    '\t<child name="Stores" '
     'key="{5209DD15-2605-40DF-A046-0A90EFB0A928}" '
     'type="Stores" '
     'typeId="{5238AF17-5FE2-48FF-8BCC-9803A975A4F4}" '
